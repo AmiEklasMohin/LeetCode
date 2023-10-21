52c3f7ee597a9f1ea5b724ac1ab987121acac34c
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 00:51:21 +0600

    13. Roman to Integer

--- roman_to_integer.cpp
+++ roman_to_integer.cpp
@@ -7,46 +7,46 @@ class Solution {
 public:
     int romanToInt(string s) {
         int integer_value = 0;
-        for (int i = s.length() - 1; i >= 0 ; --i) {
+        for (int i = (s.length() - 1); i >= 0 ; --i) {
             if (s[i] == 'I') {
                 integer_value += 1;
             } else if (s[i] == 'V') {
-                if (i > 0 && s[i-1] == 'I') {
+                if (i > 0 and s[i - 1] == 'I') {
                     integer_value += 4;
                     i -= 1;
                 } else {
                     integer_value += 5;
                 }
             } else if(s[i] == 'X') {
-                if (i > 0 && s[i-1] == 'I') {
+                if (i > 0 and s[i - 1] == 'I') {
                     integer_value += 9;
                     i -= 1;
                 } else {
                     integer_value += 10;
                 }
             } else if (s[i] == 'L') {
-                if (i > 0 && s[i-1] == 'X') {
+                if (i > 0 and s[i - 1] == 'X') {
                     integer_value += 40;
                     i -= 1;
                 } else {
                     integer_value += 50;
                 }
             } else if(s[i] == 'C') {
-                if (i > 0 && s[i-1] == 'X') {
+                if (i > 0 and s[i - 1] == 'X') {
                     integer_value += 90;
                     i -= 1;
                 } else {
                     integer_value += 100;
                 }
             } else if (s[i] == 'D') {
-                if (i > 0 && s[i-1] == 'C') {
+                if (i > 0 and s[i - 1] == 'C') {
                     integer_value += 400;
                     i -= 1;
                 } else {
                     integer_value += 500;
                 }
             } else if(s[i] == 'M') {
-                if (i > 0 && s[i-1] == 'C') {
+                if (i > 0 and s[i - 1] == 'C') {
                     integer_value += 900;
                     i -= 1;
                 } else {
