179458a3dd93ab052230eaaf18f7da536c813a16
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-21 17:38:10 +0600

    14. Longest Common Prefix

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,38 @@
+/*https://leetcode.com/problems/longest-common-prefix/*/
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string longest_string = "";
+        string temp, str = strs[0];
+        for (int i = 0; i < str.length(); ++i) {
+            for (int j = 0; j < strs.size(); ++j) {
+                temp = strs[j];
+                if (i > temp.length() || str[i] != temp[i]) {
+                    return longest_string;
+                }
+            }
+            longest_string += str[i];
+        }
+        return longest_string;
+    }
+};
+
+int main() {
+    vector<string> strs;
+    string longest_string, str;
+    int num_of_string;
+    cin >> num_of_string;
+    for (int i = 1; i <= num_of_string; ++i) {
+        cin >> str;
+        strs.push_back(str);
+    }
+    Solution ans;
+    longest_string = ans.longestCommonPrefix(strs);
+    cout << longest_string << endl;
+    return 0;
+} 
